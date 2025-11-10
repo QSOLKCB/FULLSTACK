b0ee509d737b22c90f752f6e5cfb3704ff963a6f
Trent Slade <73952179+EmergentMonk@users.noreply.github.com>
2025-11-11 08:41:11 +1030

    Add function to install core developer tools

--- stacks/devtools.sh
+++ stacks/devtools.sh
@@ -1,1 +1,6 @@
+install_devtools() {
+  color "🧰 Installing Core Developer Tools..."
+  sudo pacman -S --noconfirm --needed git neovim zsh docker podman htop btop base-devel
+  log "Dev tools installed."
+}
 
