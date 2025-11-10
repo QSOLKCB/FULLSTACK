f5e9256f7053fd78bae22af9da817ad94f925084
Trent Slade <73952179+EmergentMonk@users.noreply.github.com>
2025-11-11 08:42:40 +1030

    Add function to install science stack dependencies

--- stacks/science.sh
+++ stacks/science.sh
@@ -1,1 +1,7 @@
+install_science() {
+  color "🔬 Installing Science Stack..."
+  sudo pacman -S --noconfirm --needed python-numpy python-scipy python-pandas python-matplotlib python-seaborn \
+  python-sympy python-jupyterlab r octave gnuplot texlive-core texlive-latexextra geogebra freecad
+  log "Science stack installed."
+}
 
